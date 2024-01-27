0faaf2f71f53778795d5eada5198c4e2cc361c02
TtroubleTT <TtroubleTT2@gmail.com>
2024-01-26 23:43:35 -0800

    attacking spell api and long range spell

---
+++ GDIMSIM/Assets/Scripts/Entity/Player/Spells/Combat/CombatSpell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CombatSpell : SpellBase, ICombat
+{
+    public abstract float Damage { get; set; }
+
+    public abstract void Attack();
+    
+    public bool HurtEnemy(GameObject enemy, float damage)
+    {
+        if (!enemy.CompareTag("Enemy"))
+            return false;
+        
+        enemy.GetComponent<EnemyBase>().SubtractHealth(damage);
+        return true;
+    }
+    
+    public override bool CastSpell()
+    {
+        // Checks if Cooldown has passed
+        if (!base.CastSpell())
+            return false;
+
+        Attack();
+        return true;
+    }
+}
---
+++ GDIMSIM/Assets/Scripts/Entity/Player/Spells/Combat/SpellBlast.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellBlast : CombatSpell
+{
+    public override float Damage { get; set; }
+
+    protected override float Cooldown { get; set; }
+
+    [Header("References")] 
+    [SerializeField] private Transform camTrans;
+    [SerializeField] private GameObject projectilePrefab;
+
+    [Header("Stats")]
+    [SerializeField] private float cooldown = 10f;
+
+    [Header("Projectile Stats")] 
+    [SerializeField] private float damage = 40f;
+    [SerializeField] private float speed = 50f;
+    [SerializeField] private float range = 120f;
+    
+    private readonly Dictionary<ShootingProjectile.Stats, float> _projectileStats = new();
+
+    protected override void InitializeAbstractedStats()
+    {
+        Damage = damage;
+        Cooldown = cooldown;
+    }
+    
+    private void InitializeStats()
+    {
+        _projectileStats.Add(ShootingProjectile.Stats.Damage, Damage);
+        _projectileStats.Add(ShootingProjectile.Stats.Speed, speed);
+        _projectileStats.Add(ShootingProjectile.Stats.Range, range);
+    }
+    
+    private void Start()
+    {
+        InitializeStats();
+    }
+    
+    public override bool CastSpell()
+    {
+        // Checks if Cooldown has passed
+        if (!base.CastSpell())
+            return false;
+        
+        return true;
+    }
+
+    public override void Attack()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, camTrans.position + (camTrans.forward * 2), camTrans.rotation);
+        Vector3 direction = camTrans.forward.normalized; // Gets direction player is looking
+        projectile.GetComponent<ShootingProjectile>().ProjectileInitialize(_projectileStats, direction);
+    }
+}
