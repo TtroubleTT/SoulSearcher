c24d3aeb819b04d5dba639c8a1bfcd6d01498bfd
AmeliaTrautman <149648637+AmeliaTrautman@users.noreply.github.com>
2024-01-31 16:23:12 -0800

    wooooo

--- GDIMSIM/Assets/Scripts/Entity/Enemy/EnemyBase.cs
+++ GDIMSIM/Assets/Scripts/Entity/Enemy/EnemyBase.cs
@@ -6,10 +6,28 @@ using UnityEngine.SceneManagement;
 
 public abstract class EnemyBase : EntityBase
 {
+    EnemyStatusEffect statuseffect;
     protected virtual float SoulDropAmount { get; set; } = 10;
+
+    //referencing the status effect script so that the spells work
+    private void Start()
+    {
+        statuseffect = FindObjectOfType<EnemyStatusEffect>();
+    }
 
     protected override void Die()
     {
         Destroy(gameObject);
     }
+
+    public void CauseStatusEffect(int x)
+    {
+        if (x == 1)
+        {
+            statuseffect.Burning(this, 20, 3, 1f);
+        }
+
+    }
+
+
 }
