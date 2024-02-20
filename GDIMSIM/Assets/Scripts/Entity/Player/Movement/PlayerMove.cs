adcfce92da14a84e48515990274ad177056ff83d
TtroubleTT <TtroubleTT2@gmail.com>
2024-02-19 18:32:01 -0800

    Movement with new input system

--- GDIMSIM/Assets/Scripts/Entity/Player/Movement/PlayerMove.cs
+++ GDIMSIM/Assets/Scripts/Entity/Player/Movement/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour
@@ -13,12 +14,15 @@ public class PlayerMove : MonoBehaviour
     private Vector3 _playerVelocity;
     private bool _groundedPlayer;
 
+    private Vector2 _movementInput = Vector2.zero;
+    private bool _jumped = false;
+
     private void Start()
     {
         _controller = gameObject.GetComponent<CharacterController>();
     }
 
-    void Update()
+    private void Update()
     {
         _groundedPlayer = _controller.isGrounded;
         if (_groundedPlayer && _playerVelocity.y < 0)
@@ -26,21 +30,33 @@ public class PlayerMove : MonoBehaviour
             _playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 move = new Vector3(_movementInput.x, 0, _movementInput.y);
         _controller.Move(move * (Time.deltaTime * playerSpeed));
 
+        /*
         if (move != Vector3.zero)
         {
             gameObject.transform.forward = move;
         }
+        */
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && _groundedPlayer)
+        if (_jumped && _groundedPlayer)
         {
             _playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
         _playerVelocity.y += gravityValue * Time.deltaTime;
         _controller.Move(_playerVelocity * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        _jumped = context.action.triggered;
     }
 }
