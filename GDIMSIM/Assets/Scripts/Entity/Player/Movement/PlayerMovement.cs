285cb5013ac1486d8201114e647b757b0cb45b10
TtroubleTT <TtroubleTT2@gmail.com>
2024-02-26 00:10:32 -0800

    learning nav mesh

--- GDIMSIM/Assets/Scripts/Entity/Player/Movement/PlayerMovement.cs
+++ GDIMSIM/Assets/Scripts/Entity/Player/Movement/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
