46b6dd2e49cc43fab7599df1b62691ebed461300
TtroubleTT <TtroubleTT2@gmail.com>
2024-02-28 21:23:33 -0800

    fix enemies shooting

--- GDIMSIM/Assets/Scripts/Entity/Enemy/ShootingEnemy.cs
+++ GDIMSIM/Assets/Scripts/Entity/Enemy/ShootingEnemy.cs
@@ -70,9 +70,10 @@ public class ShootingEnemy : EnemyBase
     private bool IsInRange()
     {
         float distance = Vector3.Distance(player.transform.position, transform.position);
-
         if (distance <= shotRange)
+        {
             return true;
+        }
 
         return false;
     }
