c5e971bc9f3ebabc0c7728be3a5a9acf5e095bbc
AmeliaTrautman <149648637+AmeliaTrautman@users.noreply.github.com>
2024-03-11 20:29:52 -0700

    omg

--- GDIMSIM/Assets/Scripts/Building/Corral.cs
+++ GDIMSIM/Assets/Scripts/Building/Corral.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Corral : MonoBehaviour
 {
+    private SoulCounter _soulCounter;
+
     // [SerializeField] private float maxSoul = 5;
     [SerializeField] private float currentSoul = 0;
 
@@ -11,10 +13,17 @@ public class Corral : MonoBehaviour
 
     [SerializeField] private Vector3 spawnPoint;
 
+    private void Start()
+    {
+        _soulCounter = FindObjectOfType<SoulCounter>();
+    }
+
+
     public void AddSoul(float amount)
     {
         currentSoul = currentSoul + amount;
         Instantiate(soulObject, spawnPoint, Quaternion.identity);
+        _soulCounter.DecreaseSoulCount();
     }
 
     public void SubtractSoul(float amount)
